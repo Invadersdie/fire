347d4e876e2f1cae728c2af6923f99443a64d3a1
Invadersdie <mlevdikov@gmail.com>
2017-11-27 23:00:37 +0300

    ADD_ADC
    Added ADC, NOT TRUE VALUE

--- main.cpp
+++ main.cpp
@@ -9,58 +9,81 @@ void timer1_init()
 	TCCR1B |= (1 << CS10);
 	TCCR1B |= (1 << CS12);
 	// enable overflow interrupt
-	TIMSK1 |= (1 << TOIE1);
+	// TIMSK1 |= (1 << TOIE1);
+}
+void adc0_init()
+{
+  	// http://samou4ka.net/page/analogo-cifrovoj-preobrazovatel-mk-atmega8
+	ADMUX=0; //#PORT
+	ADCSR=0x86; //0b10000110
+}
+double newAngle()
+{
+	ADCSR |= 0x40;
+	data = ADCW;
+	return //#TODO
 }
 
 int main(void) {
-
-	PORTD |= (1 << 3)|(1 << 2);
-	DDRB = 0b00000011;
-	double angle = PIND1;     // #TODO POTENCIOMETR FOR *ANGLE*
-	double sparkDelay = angle;
-	timer1_init();
+	
+	bool temp;
+	double angle1;
 	double forDelay;
 	
+	PORTD |= (1 << 3)|(1 << 2);
+	DDRB = 0b00000011;
+	
+	timer1_init();
+	adc_init();
+	double angle = newAngle();     // #TODO POTENCIOMETR FOR *ANGLE*
+	double sparkDelay = angle;
+		
 	while (1) {
+	
 		if (PIND2) {
+			temp = false;
 			TCNT1 = 0;   //#Timer ON
 			_delay_ms(sparkDelay);
 			PORTB = 0b00000010;
 			while (PIND2) {
+				if(!temp){angle1 = newAngle();temp=true;};
 			}
-			forDelay = TCNT1;
+			forDelay = TCNT1-angle;
 			continue;
 		}
 		if (PIND3) {
+			temp = false;
 			TCNT1 = 0; //#Timer ON
 			_delay_ms(sparkDelay);
 			PORTB = 0b00000001;
 			while (PIND3) {
+				if(!temp){angle1 = newAngle();temp=true;};
 			}
-			forDelay = TCNT1;
+			forDelay = TCNT1-angle;
 			continue;
 		}
 	
-		if (forDelay > 0x0775) {sparkDelay = angle+ 0x22; break; };
-		if (forDelay > 0x0598) {sparkDelay = angle+ 0x14; break; };
-		if (forDelay > 0x0459) {sparkDelay = angle+ 0x0E; break; };
-		if (forDelay > 0x03BA) {sparkDelay = angle+ 0x0A; break; };
-		if (forDelay > 0x0332) {sparkDelay = angle+ 0x08; break; };
-		if (forDelay > 0x02CC) {sparkDelay = angle+ 0x05; break; };
-		if (forDelay > 0x027C) {sparkDelay = angle+ 0x04; break; };
-		if (forDelay > 0x023C) {sparkDelay = angle+ 0x04; break; };	
-		if (forDelay > 0x0208) {sparkDelay = angle+ 0x03; break; };	
-		if (forDelay > 0x01DD) {sparkDelay = angle+ 0x02; break; };
-		if (forDelay > 0x01B8) {sparkDelay = angle+ 0x03; break; };
-		if (forDelay > 0x0199) {sparkDelay = angle+ 0x01; break; };
-		if (forDelay > 0x017D) {sparkDelay = angle+ 0x02; break; };
-		if (forDelay > 0x0165) {sparkDelay = angle+ 0x01; break; };
-		if (forDelay > 0x0150) {sparkDelay = angle+ 0x02; break; };
-		if (forDelay > 0x013E) {sparkDelay = angle+ 0x01; break; };
-		if (forDelay > 0x012D) {sparkDelay = angle+ 0x01; break; };
+		if (forDelay > 0x0775) {sparkDelay = angle + 0x22; break; };
+		if (forDelay > 0x0598) {sparkDelay = angle + 0x14; break; };
+		if (forDelay > 0x0459) {sparkDelay = angle + 0x0E; break; };
+		if (forDelay > 0x03BA) {sparkDelay = angle + 0x0A; break; };
+		if (forDelay > 0x0332) {sparkDelay = angle + 0x08; break; };
+		if (forDelay > 0x02CC) {sparkDelay = angle + 0x05; break; };
+		if (forDelay > 0x027C) {sparkDelay = angle + 0x04; break; };
+		if (forDelay > 0x023C) {sparkDelay = angle + 0x04; break; };	
+		if (forDelay > 0x0208) {sparkDelay = angle + 0x03; break; };	
+		if (forDelay > 0x01DD) {sparkDelay = angle + 0x02; break; };
+		if (forDelay > 0x01B8) {sparkDelay = angle + 0x03; break; };
+		if (forDelay > 0x0199) {sparkDelay = angle + 0x01; break; };
+		if (forDelay > 0x017D) {sparkDelay = angle + 0x02; break; };
+		if (forDelay > 0x0165) {sparkDelay = angle + 0x01; break; };
+		if (forDelay > 0x0150) {sparkDelay = angle + 0x02; break; };
+		if (forDelay > 0x013E) {sparkDelay = angle + 0x01; break; };
+		if (forDelay > 0x012D) {sparkDelay = angle + 0x01; break; };
 		if (forDelay > 0x011E) {sparkDelay = angle; break; };
 	
 		sparkDelay = angle; //default value
+		angle = angle1;
 	}                                                  /* End event loop */
 	
 	return 0;
