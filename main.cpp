9eb92a85d52ba815c8aca3f4c18a7f6986b2f8fd
Invadersdie <mlevdikov@gmail.com>
2017-11-27 15:47:57 +0300

    New

--- main.cpp
+++ main.cpp
@@ -1,39 +1,55 @@
-/*
-   Demo of the simplest on/off button code
-   Button connected to PD2
-   LEDs connected to PB0..PB7
-*/
-
-// ------- Preamble -------- //
+#define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-#define SPARKDELAY 500
-#define SPARKTIME 500
+
+void timer1_init()
+{
+	TCCR1A = 0;     // set entire TCCR1A register to 0
+	TCCR1B = 0;     // same for TCCR1B
+	TCCR1B |= (1 << CS10);
+	TCCR1B |= (1 << CS12);
+	// enable overflow interrupt
+	TIMSK1 |= (1 << TOIE1);
+}
+
 int main(void) {
-  // -------- Inits --------- //
-  PORTD |= (1 << PD3)|(1 << PD2);  /* initialize pullup resistor on our input pin */
-  DDRB = 0xff;                           /* set up all LEDs for output */
-  bool sparkA=false;
-  bool sparkB=false;
-  // ------ Event loop ------ //
-  while (1) {
-    PORTB = 0b00000000;
-    if (bit_is_clear(PIND, PD2)&&(!sparkA)) {            /* look for button press */
-      sparkA=!sparkA;
-      _delay_ms(SPARKDELAY);
-      PORTB = 0b00000001;
-      _delay_ms(SPARKTIME);
-      continue;
-    }
-    if(bit_is_clear(PIND, PD3)&&(!sparkB)){
-      sparkB=!sparkB;
-      _delay_ms(SPARKDELAY);
-      PORTB = 0b00000010;
-      _delay_ms(SPARKTIME);
-      continue;
-    }
-    sparkA=false;
-    sparkB=false;
-  }                                                  /* End event loop */
-  return 0;
+
+	PORTD |= (1 << 3)|(1 << 2);
+	DDRB = 0xff;
+	double angle = 0;     // #TODO POTENCIOMETR FOR *ANGLE*
+	double sparkDelay = angle;
+	timer1_init();
+	double forDelay;
+	
+	while (1) {
+		PORTB = 0b00000011;
+		if (PIND2) {
+			TCNT1 = 0;   //#Timer ON
+			_delay_ms(sparkDelay);
+			PORTB = 0b00000010;
+			while (PIND2) {
+			}
+			forDelay = TCNT1;
+			continue;
+		}
+		
+		if (PIND3) {
+			TCNT1 = 0; //#Timer ON
+			_delay_ms(sparkDelay);
+			PORTB = 0b00000001;
+			while (PIND3) {
+			}
+			forDelay = TCNT1;
+			//#Timer Off
+			continue;
+		}
+		
+		if (forDelay < 5) {sparkDelay = angle+ 1; break; };
+		if (forDelay < 4) {sparkDelay = angle+ 2; break; };
+		if (forDelay < 3) {sparkDelay = angle+ 3; break; };
+		if (forDelay < 2) {sparkDelay = angle+ 4; break; };
+		sparkDelay = angle; //default value
+	}                                                  /* End event loop */
+	
+	return 0;
 }
