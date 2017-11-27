502baf3941b14d109ee34f07a399c199530ba9f8
Invadersdie <mlevdikov@gmail.com>
2017-11-27 11:42:26 +0300

    Update main.cpp
    Change

--- main.cpp
+++ main.cpp
@@ -1,41 +1,42 @@
-/*
-   Demo of the simplest on/off button code
-   Button connected to PD2
-   LEDs connected to PB0..PB7
-*/
-
-// ------- Preamble -------- //
 #include <avr/io.h>
 #include <util/delay.h>
-#define SPARKDELAY 500
-#define SPARKTIME 500
+
+
 int main(void) {
   // -------- Inits --------- //
   PORTD |= (1 << PD3)|(1 << PD2);  /* initialize pullup resistor on our input pin */
   DDRB = 0xff;                           /* set up all LEDs for output */
-  bool sparkA=false;
-  bool sparkB=false;
-  // ------ Event loop ------ //
+  ?? sparkDelay;  // TYPE "??"
+
   while (1) {
-    PORTB = 0b00000000;
-    if (bit_is_clear(PIND, PD2)) {            /* look for button press */
-      //Timer ON
-       _delay_ms(SPARKDELAY);      
-         while {bit_is_clear(PIND, PD2) {
-            PORTB = 0b00000001;
+    PORTB = 0b00000011;
+    if (bit_is_clear(PIND, PD2)) {            
+      //#Timer ON
+       _delay_ms(sparkDelay);  
+		PORTB = 0b00000010;	   
+         while (bit_is_clear(PIND, PD2)) {
             }
+      //#Timer Off
       continue;
     }
-    if(bit_is_clear(PIND, PD3)&&(!sparkB)){
-      sparkB=!sparkB;
-      _delay_ms(SPARKDELAY);
-      PORTB = 0b00000010;
-      _delay_ms(SPARKTIME);
+	
+    if (bit_is_clear(PIND, PD3)) {            
+      //#Timer ON
+       _delay_ms(sparkDelay);      
+	   PORTB = 0b00000001;
+         while (bit_is_clear(PIND, PD3)) {
+            }
+      //#Timer Off
       continue;
-    }
-   //After all shit check timer and set SPARKDELAY
-    sparkA=false;
-    sparkB=false;
+		 }
+	while (true){
+	if (timer_Timer < ??) {sparkDelay = ??; break; };
+	if (timer_Timer < ??) {sparkDelay = ??; break; };
+	if (timer_Timer < ??) {sparkDelay = ??; break; };
+	if (timer_Timer < ??) {sparkDelay = ??; break; };
+//default value 
+	sparkDelay = ??;
+	}
   }                                                  /* End event loop */
   return 0;
 }
