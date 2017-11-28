0bee230873335c8cdab552a1129dd9fdee50a584
Invadersdie <mlevdikov@gmail.com>
2017-11-28 11:09:46 +0300

    Error Fix

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void timer1_init()
-{
+void timer1_init() {
 	TCCR1A = 0;     // set entire TCCR1A register to 0
 	TCCR1B = 0;     // same for TCCR1B
 	TCCR1B |= (1 << CS10);
@@ -11,8 +10,7 @@ void timer1_init()
 	// enable overflow interrupt
 	// TIMSK1 |= (1 << TOIE1);
 }
-void adc0_init()
-{
+void adc0_init() {
 	// http://samou4ka.net/page/analogo-cifrovoj-preobrazovatel-mk-atmega8
 	ADMUX=0; //#PORT
 	ADCSRA=0x86; //0b10000110
@@ -23,7 +21,7 @@ int newAngle() {
 	int data = (int)(ADCW/4);
 	return data;
 }
-void _delay_us_my (int delay){
+void _delay_us_my (int delay) {
 	if (delay != 0){
 		for (int i = 0; i<delay; i++){
 			_delay_us(1);
@@ -55,8 +53,7 @@ int main(void) {
 			while (PIND2) {
 				if(!temp){angle1 = newAngle();temp=true;};
 			}
-			forDelay = -angle;
-			continue;
+			forDelay = TCNT1;
 		}
 		
 		if (PIND3) {
@@ -69,30 +66,28 @@ int main(void) {
 				if(!temp){angle = newAngle();temp=true;};
 			}
 			forDelay = TCNT1;
-			continue;
 		}
 		
 		if      (forDelay > 0x0775) {sparkDelay = 204;}
-		else if (forDelay > 0x0598) {sparkDelay = 120;}
-		else if (forDelay > 0x0459) {sparkDelay = 84;}
-		else if (forDelay > 0x03BA) {sparkDelay = 60;}
-		else if (forDelay > 0x0332) {sparkDelay = 48;}
-		else if (forDelay > 0x02CC) {sparkDelay = 30;}
-		else if (forDelay > 0x027C) {sparkDelay = 24;}
-		else if (forDelay > 0x023C) {sparkDelay = 24;}
-		else if (forDelay > 0x0208) {sparkDelay = 18;}
-		else if (forDelay > 0x01DD) {sparkDelay = 12;}
-		else if (forDelay > 0x01B8) {sparkDelay = 11;}
-		else if (forDelay > 0x0199) {sparkDelay = 10;}
-		else if (forDelay > 0x017D) {sparkDelay = 8;}
-		else if (forDelay > 0x0165) {sparkDelay = 6;}
-		else if (forDelay > 0x0150) {sparkDelay = 4;}
-		else if (forDelay > 0x013E) {sparkDelay = 2;}
-		else if (forDelay > 0x012D) {sparkDelay = 1;}
-		else if (forDelay > 0x011E) {sparkDelay = 0;}
-		else sparkDelay = 0; //default value
+			else if (forDelay > 0x0598) {sparkDelay = 120;}
+			else if (forDelay > 0x0459) {sparkDelay = 84;}
+			else if (forDelay > 0x03BA) {sparkDelay = 60;}
+			else if (forDelay > 0x0332) {sparkDelay = 48;}
+			else if (forDelay > 0x02CC) {sparkDelay = 30;}
+			else if (forDelay > 0x027C) {sparkDelay = 24;}
+			else if (forDelay > 0x023C) {sparkDelay = 24;}
+			else if (forDelay > 0x0208) {sparkDelay = 18;}
+			else if (forDelay > 0x01DD) {sparkDelay = 12;}
+			else if (forDelay > 0x01B8) {sparkDelay = 11;}
+			else if (forDelay > 0x0199) {sparkDelay = 10;}
+			else if (forDelay > 0x017D) {sparkDelay = 8;}
+			else if (forDelay > 0x0165) {sparkDelay = 6;}
+			else if (forDelay > 0x0150) {sparkDelay = 4;}
+			else if (forDelay > 0x013E) {sparkDelay = 2;}
+			else if (forDelay > 0x012D) {sparkDelay = 1;}
+			else if (forDelay > 0x011E) {sparkDelay = 0;}
+			else sparkDelay = 0; //default value
 		
 		}                                                  /* End event loop */
-		
-		return 0;
+	return 0;
 	}
